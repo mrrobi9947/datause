cd483dd8e9ea6e81c128b2eb2ce4322e74f4947d
mrrobi9947 <89423131+mrrobi9947@users.noreply.github.com>
2021-08-24 02:55:52 +0530

    Create Database.py

---
+++ Database.py
@@ -0,0 +1,9 @@
+print('/^^^^^^hqckbox\^^^^^^^^^^' * 100)
+
+var1='this tool will help tou to find your data balance'
+var2='enter your data balance below'
+print(var1)
+print(var2)
+data_balance = input ('what id your data balance?  ')
+data = 1500 - int(data_balance)
+print(data)
